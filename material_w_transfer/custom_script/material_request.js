3e8557205a2e7749d5aa87f7df99e8e5cd452f7e
vicky <vignesh@crisoconsulting.in>
2018-09-04 12:15:31 +0530

    first commit

--- material_w_transfer/custom_script/material_request.js
+++ material_w_transfer/custom_script/material_request.js
@@ -1,7 +1,7 @@
 
 frappe.ui.form.on('Material Request', {
 	refresh: function (frm) {
-		if(frm.doc.status === "Pending" && frm.doc.docstatus == 1){
+		if(frm.doc.status === "Pending" && frm.doc.docstatus == 1 && frm.doc.material_request_type == "Material Transfer"){
 			cur_frm.add_custom_button(__("Make Stock Trasnfer"), function(){
 				frappe.call({
 					method: 'material_w_transfer.material_warehouse_transfer.doctype.stock_transfer.stock_transfer.make_stock_transfer',
